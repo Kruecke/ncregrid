446c6ac00dc783573336c00acc22eb87bff13fed
Florian Klemme <mail@florianklemme.de>
2015-12-14 14:16:15 +0100

    First try to build in conda environment.

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-# Copy prebuild binary to prefix/bin/
+# Switch to ncregrid directory
+cd "$SRC_DIR/ncregrid"
+# Build ncregrid
+make all install
+
+# Copy executable to prefix/bin/
 mkdir -p "$PREFIX/bin"
 cp "$SRC_DIR/ncregrid/bin/ncregrid" "$PREFIX/bin/ncregrid"
 # Make sure the binary is executable
